8c13fb7867450f2a237c6df1b11ca35e27b5e52e
BaldwinMichelson <BaldwinMichelson@users.noreply.github.com>
2026-03-31 10:43:53 +0800

    Polish deployment config

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -12,6 +12,3 @@ export const wagmiConfig = createConfig({
 // TODO(builder-code): append the official Base builder code data suffix here.
 // Replace the placeholder below once the final builder code is provided.
 // const BUILDER_CODE_SUFFIX = "TODO_REPLACE_WITH_BUILDER_CODE_SUFFIX";
-
-
-
